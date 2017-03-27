5c37a58372fea0d71677d258a06941208560eb50
Dheeraj Kaushik <dheeraj@dezyne.in>
2017-03-27 12:11:52 +0530

    Added monkey-cage.de DB details

--- admin/db.php
+++ admin/db.php
@@ -1,7 +1,8 @@
 <?php
 //$servername = "rdbms.strato.de";
 $servername = "localhost";
-$username = "U2700106";
+//$username = "U2700106";
+$username = "U2924911";
 $password = "monkeycage098";
 
 
@@ -14,7 +15,8 @@ if (!$conn) {
 }
 
 // make foo the current db
-$db_selected = mysql_select_db('DB2700106', $conn);
+//$db_selected = mysql_select_db('DB2700106', $conn);
+$db_selected = mysql_select_db('DB2924911', $conn);
 if (!$db_selected) {
     die ('Can\'t use foo : ' . mysql_error());
 }
